0cfa98c3f409fb2331c70cf965048a1c7965eb21
Stuart Mackenzie <ilovediscostu@gmail.com>
2026-04-11 19:51:51 +0100

    updating icon config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,13 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://updownpod-79a0d.web.app',
-  integrations: [mdx(), astroImageTools, icon(), sitemap(), react()],
+  integrations: [
+    mdx(),
+    astroImageTools,
+    icon({
+      iconDir: 'src/assets/icons',
+    }),
+    sitemap(),
+    react(),
+  ],
 });
